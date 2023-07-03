e88b252db02b55c7f14ee560f67d77134213abd3
Feihong Hsu <feihong.hsu@gmail.com>
2023-07-03 11:07:19 -0500

    Render sketches right above their corresponding code fences

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,43 @@
+const markdownIt = require("markdown-it");
+
+function unescapeHtml(s) {
+	return s
+		.replace(/&amp;/g, "&")
+		.replace(/&lt;/g, "<")
+		.replace(/&gt;/g, ">")
+		.replace(/&quot;/g, "\"");
+}
+
+function wrapWithP5Constructor(code, sketchId) {
+	return `new p5(s => {\n${code}\n}, document.getElementById("${sketchId}"));`;
+}
+
+function updateFenceRenderer(md) {
+	const origRenderer = md.renderer.rules.fence;
+
+	const newRenderer = function(tokens, idx, options, env, self) {
+		const html = origRenderer(tokens, idx, options, env, self);
+		// Get rid of markup at front end end, unescape HTML entities
+		let code = unescapeHtml(html.substring(11, html.length - 14));
+		const sketchId = `sketch-${newRenderer.count}`
+    code = wrapWithP5Constructor(code, sketchId);
+		newRenderer.count += 1;
+		const script = `<div id="${sketchId}"></div>\n<script>\n${code}\n</script>\n`;
+		return script + html;
+	};
+	newRenderer.count = 1;
+
+	md.renderer.rules.fence = newRenderer;
+}
+
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("p5.min.js");
 	eleventyConfig.addPassthroughCopy("main.css");
+
+	const md = markdownIt({
+		html: true,
+		linkify: true,
+	});
+	updateFenceRenderer(md);
+	eleventyConfig.setLibrary("md", md);
 };
--- get-started.md
+++ get-started.md
@@ -3,23 +3,18 @@ layout: sketch.njk
 title: Get Started
 ---
 
-<div id=sketch></div>
+Source: https://p5js.org/get-started/
 
-<div id=code></div>
+```
+s.setup = function() {
+  s.createCanvas(400, 400);
+};
+
+s.draw = function() {
+  s.background(140, 150, 230);
+  s.ellipse(200, 100, 100, 100);
+};
+```
 
 Links:
-- [Get started](https://p5js.org/get-started/)
 - [P5.js downloads](https://p5js.org/download/)
-
-<script>
-new p5(s => {
-  s.setup = function() {
-    s.createCanvas(400, 400);
-  }
-
-  s.draw = function() {
-    s.background(140, 150, 230);
-    s.ellipse(200, 100, 100, 100);
-  }
-}, document.getElementById('sketch'))
-</script>
